a103dd6ba0d2b77503152d5f78101e21f1850358
unknown <guilhermecprocopio@gmail.com>
2023-09-18 19:28:13 -0300

    adding control-node config

---
+++ ansible/control-node/provision.sh
@@ -0,0 +1,15 @@
+#!bin/sh
+
+# installing ansible
+sudo yum -y install epel-release
+echo "installing ansible"
+sudo yum -y install ansible
+
+# adding hosts
+cat <<EOT >> /etc/hosts
+192.168.1.2 control-node
+192.168.1.3 app01
+192.168.1.4 db01
+EOT
+
+
